0d4980c70a4da57a488d7402fd12efe8986d9e57
Juan E. M <>
2023-05-31 14:52:42 -0700

    lab con tarea

--- EscobarSnapchat/ViewController.swift
+++ EscobarSnapchat/ViewController.swift
@@ -8,8 +8,11 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import GoogleSignIn
 
 class iniciarSessionViewController: UIViewController {
+    
+    
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -24,10 +27,42 @@ class iniciarSessionViewController: UIViewController {
         }
     }
     
+    
+    
+    
+    
+    @IBAction func sesionGoogleTapped(_ sender: Any) {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) {[unowned self] result, error in
+            guard error == nil else {
+                return
+            }
+            guard let user = result?.user,
+                  let idToken = user.idToken?.tokenString
+            else {
+                return
+            }
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print("Ocurrió un error: \(error)")
+                    return
+                } else {
+                    print("Logeo por Google de forma correcta")
+                }
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Configuration for google auth
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+       
     }
+  
 
 
 }
